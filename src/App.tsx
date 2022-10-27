1f23a377d6f4e3cd7a4ea709c5efbfc2b23d4c50
Nanamsala <pdemanansala@gmail.com>
2022-10-28 03:35:56 +0800

    Fixed Search Bar Function

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { ChangeEvent, FormEvent, useRef, useState } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [items, setItems] = useState<string[]>(["aAa","aaa"])
-  const [filtereditems, setFilteredItems] = useState<string[]>(items)
+  const [allItems, setItems] = useState<string[]>(["Banana","Apple"])
+  const [query, setQuery] = useState<string>("")
   const inputRef = useRef<HTMLInputElement>(null)
+  
+  const filteredItems:string[] = allItems.filter(item =>
+    item.toLowerCase().includes(query.toLowerCase())
+  )
+
   function onSubmit(e:FormEvent){
     e.preventDefault()
-    if(inputRef===null || inputRef.current === null || inputRef.current.value=== "") return
+    if(!inputRef.current || inputRef.current.value=== "") return
     const val = inputRef.current.value
-    setItems(data => {
-      return [...data, val]
-    })
+    setItems( [...allItems, val]
+    )
     inputRef.current.value = ""
   }
 
-  function onChange (e:ChangeEvent<HTMLInputElement>){
-    const text = e.target.value
-    setFilteredItems(items.filter(item => item.toLowerCase().includes(text.toLowerCase())))
 
-  }
   return (
     <div className="App">
       Search: 
-      <input onChange={onChange} type="search"/>
+      <input value={query} onChange={e => setQuery(e.target.value)} type="search"/>
       <form onSubmit={onSubmit}>
         New Item: <input ref={inputRef} type="text"/>
         <button type="submit">Add</button>
       </form>
       <h3>Items:</h3>
-      {filtereditems.map(item => (
+      {filteredItems.map(item => (
         <div>{item}</div>
       ))}
 
